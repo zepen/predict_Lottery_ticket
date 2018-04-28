9130a7acfbf4d98233ddc3c5686ba4aae7c9bda4
Niuzepeng <18217127859@163.com>
2018-04-28 17:43:57 +0800

    first

--- lstm_model.py
+++ lstm_model.py
@@ -7,20 +7,18 @@ import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-from keras.optimizers import SGD
+from keras.optimizers import Adam
 from keras.utils import to_categorical
-from multiprocessing import Pool
 
 DATA = pd.read_csv("data/data.csv")
 COL_NAME = ["红球号码_1", "红球号码_2", "红球号码_3",
             "红球号码_4", "红球号码_5", "红球号码_6", "蓝球"]
 
-X_DATA = []
-Y_DATA = []
 
-
-def transform_data(col_name):
-    data_list = DATA[col_name].tolist()
+def transform_data():
+    data_list = []
+    for col_name in COL_NAME:
+        data_list.extend(DATA[col_name].tolist())
     data_list_len = len(data_list)
     end_index = int(np.float(data_list_len / float(4)) * 4)
     data_list.reverse()
@@ -34,25 +32,25 @@ def transform_data(col_name):
 
 
 def create_model_data():
-    for col_name in COL_NAME:
-        data = transform_data(col_name)
-        X_DATA.append(data[:, 0:3].reshape([-1, 3, 1]))
-        Y_DATA.append(to_categorical(data[:, 3:].ravel()))
+    data = transform_data()
+    x_data = data[:, 0:3].reshape([-1, 3, 1])
+    y_data = to_categorical(data[:, 3:].ravel())
+    return x_data, y_data
 
 
-def train_model(x):
-    n_class = Y_DATA[x].shape[1]
+def train_model(x_data, y_data):
+    n_class = y_data.shape[1]
+    print("The x_data shape is {}".format(x_data.shape))
+    print("The y_data shape is {}".format(y_data.shape))
     model = Sequential()
-    model.add(LSTM(10, input_shape=(3, 1), kernel_initializer='random_uniform'))
+    model.add(LSTM(32, input_shape=(3, 1), return_sequences=True))
+    model.add(LSTM(32, recurrent_dropout=0.2))
     model.add(Dense(n_class, activation="softmax"))
-    sgd = SGD(lr=0.1)
-    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=["accuracy"])
-    model.fit(X_DATA[x], Y_DATA[x], batch_size=1, epochs=5000, verbose=0)
-    model.save("model/" + "model_" + str(x) + ".h5")
+    adam = Adam()
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=["accuracy"])
+    model.fit(x_data, y_data, batch_size=1, epochs=10000, verbose=1)
+    model.save("model/" + "lstm_model.h5")
 
 if __name__ == '__main__':
-    create_model_data()
-    pool = Pool(4)
-    pool.map(train_model, range(7))
-    pool.close()
-    pool.join()
+    x_train, y_train = create_model_data()
+    train_model(x_train, y_train)
