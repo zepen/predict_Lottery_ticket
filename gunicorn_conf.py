edc109f1e06998e0ccce1accfab31d2acdc836a0
Bigcat <18217127859@163.com>
2022-05-29 00:32:07 +0800

    修改flask版本 限定worker

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -4,8 +4,9 @@ backlog = 512
 timeout = 300
 worker_class = 'gevent'
 
-workers = multiprocessing.cpu_count() // 2 + 1
-threads = 2
+# workers = multiprocessing.cpu_count() // 2 + 1
+workers = 5
+threads = 1
 loglevel = 'info'
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
 
