5c7f3bfec6e283f37f4667da903a07f6bc02f82c
Bigcat <18217127859@163.com>
2022-11-06 05:37:43 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-env=$1
-echo "env args is：$1"
 processName='gunicorn'
 PID=$(ps -elf |grep $processName|grep -v grep |head -n 1 |awk '{printf $4}')
 if [ $? -eq 0 ] && [ ${#PID} -gt 0 ]; then
@@ -16,3 +14,4 @@ else
     echo "process $processName not exist"
 fi
 nohup gunicorn -c gunicorn_conf.py run_api:app &> ./logs/uwsgi.log &
+echo "gunicorn servie is running!"
