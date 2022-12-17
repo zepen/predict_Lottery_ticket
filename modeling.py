dba3144bfa21ee2065b166ce829da1ffded5e684
KittenCN <owdely@vip.163.com>
2022-12-17 08:42:06 +0800

    Use CudnnLSTM instead of LSTM in GPU computation

--- modeling.py
+++ modeling.py
@@ -7,13 +7,16 @@ import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 from tensorflow_addons.text.crf import crf_decode, crf_log_likelihood
 
-# 关闭eager模式
-tf.compat.v1.disable_eager_execution()
-tf.compat.v1.experimental.output_all_intermediates(True)
-
 gpus = tf.config.list_physical_devices("GPU")
 if gpus:
     tf.config.experimental.set_memory_growth(gpus[0],True)
+    from tensorflow.compat.v1.keras.layers import CuDNNLSTM as LSTM
+else:
+    from tensorflow.keras.layers import LSTM
+
+# 关闭eager模式
+tf.compat.v1.disable_eager_execution()
+tf.compat.v1.experimental.output_all_intermediates(True)
 
 
 class LstmWithCRFModel(object):
@@ -30,15 +33,16 @@ class LstmWithCRFModel(object):
         self._sequence_length = tf.keras.layers.Input(
             shape=(), batch_size=batch_size, dtype=tf.int32, name="sequence_length"
         )
+        
         # 构建特征抽取
         embedding = tf.keras.layers.Embedding(words_size, embedding_size)(self._inputs)
         first_lstm = tf.convert_to_tensor(
-            [tf.keras.layers.LSTM(hidden_size)(embedding[:, :, i, :]) for i in range(ball_num)]
+            [LSTM(hidden_size)(embedding[:, :, i, :]) for i in range(ball_num)]
         )
         first_lstm = tf.transpose(first_lstm, perm=[1, 0, 2])
         second_lstm = None
         for _ in range(layer_size):
-            second_lstm = tf.keras.layers.LSTM(hidden_size, return_sequences=True)(first_lstm)
+            second_lstm = LSTM(hidden_size, return_sequences=True)(first_lstm)
         self._outputs = tf.keras.layers.Dense(n_class)(second_lstm)
         # 构建损失函数
         self._log_likelihood, self._transition_params = crf_log_likelihood(
@@ -91,10 +95,13 @@ class SignalLstmModel(object):
             shape=(n_class, ), batch_size=batch_size, dtype=tf.float32, name="tag_indices"
         )
         embedding = tf.keras.layers.Embedding(outputs_size, embedding_size)(self._inputs)
-        lstm = tf.keras.layers.LSTM(hidden_size, return_sequences=True)(embedding)
+        lstm = LSTM(hidden_size, return_sequences=True)(embedding)
         for _ in range(layer_size):
-            lstm = tf.keras.layers.LSTM(hidden_size, return_sequences=True)(lstm)
-        final_lstm = tf.keras.layers.LSTM(hidden_size, recurrent_dropout=0.2)(lstm)
+            lstm = LSTM(hidden_size, return_sequences=True)(lstm)
+        if gpus:
+            final_lstm = LSTM(hidden_size)(lstm)
+        else:
+            final_lstm = LSTM(hidden_size, recurrent_dropout=0.2)(lstm)
         self._outputs = tf.keras.layers.Dense(outputs_size, activation="softmax")(final_lstm)
         # 构建损失函数
         self._loss = - tf.reduce_sum(self._tag_indices * tf.math.log(self._outputs))
